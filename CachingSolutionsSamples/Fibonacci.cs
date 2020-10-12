59906d637aee8884ba8e97248514319114818c46
Aziza Karayeva <Aziza_Karayeva@epam.com>
2020-10-12 12:22:34 +0600

    Добавьте файлы проекта.

---
+++ CachingSolutionsSamples/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using Cache.Infrastructure;
+using System;
+
+namespace CachingSolutionsSamples
+{
+    public class Fibonacci
+    {
+        private readonly ICache<int> _cache;
+
+        public Fibonacci(ICache<int> cache)
+        {
+            _cache = cache;
+        }
+
+        public int Count(int index)
+        {
+            if (index <= 0)
+            {
+                throw new ArgumentException($"{nameof(index)} must be positive number");
+            }
+
+            if (index == 1 || index == 2)
+            {
+                return 1;
+            }
+
+            int cacheValue = _cache.Get(index.ToString());
+
+            if (cacheValue != default(int))
+            {
+                Console.WriteLine($"From cache: {cacheValue}");
+                return cacheValue;
+            }
+
+            int result = Count(index - 1) + Count(index - 2);
+            Console.WriteLine($"Computed: {result}");
+
+            _cache.Set(index.ToString(), result, DateTimeOffset.Now.AddMilliseconds(300));
+            return result;
+        }
+    }
+}
---
+++ CachingSolutionsSamples/Tests1.cs
@@ -0,0 +1,37 @@
+﻿using Cache.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace CachingSolutionsSamples
+{
+    [TestClass]
+    public class Tests1
+    {
+        private readonly string _fibonacciPrefix = "_fibonacci";
+
+        [TestMethod]
+        public void FibonacciMemoryCache()
+        {
+            var fibonacci = new Fibonacci(new MemoryCache<int>(_fibonacciPrefix));
+
+            for (var i = 1; i < 20; i++)
+            {
+                Console.WriteLine(fibonacci.Count(i));
+                Thread.Sleep(100);
+            }
+        }
+
+        [TestMethod]
+        public void FibonacciRedisCache()
+        {
+            var fibonacci = new Fibonacci(new RedisCache<int>("127.0.0.1", _fibonacciPrefix));
+
+            for (var i = 1; i < 20; i++)
+            {
+                Console.WriteLine(fibonacci.Count(i));
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
